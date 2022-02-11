22ee7c889feb23908be0a87a3a6954fb88a0a1cc
Venermars <97808163+Venermars@users.noreply.github.com>
2022-02-11 21:08:25 +0300

    comlete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let strN = String(n);
+    let result = '';
+    
+    strN[0] === '-' ? strN = strN.substring(1) : null
+
+    for (i = 0; i < strN.length; i++) {
+        result = strN[i] + result;
+    }
+
+    strN[0] === '0' ? strN = strN.substring(1) : null
+
+    return result;
+
 }
